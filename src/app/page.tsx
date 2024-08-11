90e49e05290ab276e0d518dc654f70a848c56ba4
Mukul Anand Bhatt <onlyforwork941@gmail.com>
2024-08-11 16:55:02 +0530

    initial profile changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -124,7 +124,7 @@ export default function Page() {
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   I&apos;ve worked on a variety of projects, from simple
-                  websites to complex web applications. Here are a few of my
+                  websites to complex AIML Heavy applications. Here are a few of my
                   favorites.
                 </p>
               </div>
